4b2c16afdfb67b3a0afc289a9b41560a8f39f720
Maarten de Vries <maarten@de-vri.es>
2015-05-11 22:55:43 +0200

    Log node name and severity.
    refs #871

--- src/dr_log.cpp
+++ src/dr_log.cpp
@@ -1,14 +1,16 @@
 #include <utility>
+#include <type_traits>
+#include <iomanip>
 
 #include "dr_log.hpp"
-#include <boost/log/attributes/value_extraction.hpp>
+
 #include <boost/log/core.hpp>
+#include <boost/log/expressions.hpp>
 #include <boost/log/sinks.hpp>
 #include <boost/log/utility/setup.hpp>
-#include <boost/log/expressions.hpp>
 #include <boost/log/sources/logger.hpp>
-#include <boost/log/common.hpp>
 #include <boost/log/support/date_time.hpp>
+#include <boost/log/attributes/value_extraction.hpp>
 #include <boost/date_time/posix_time/posix_time_types.hpp>
 
 #include <boost/filesystem.hpp>
@@ -16,75 +18,127 @@
 
 namespace dr {
 
-namespace log      = boost::log;
-namespace keywords = boost::log::keywords;
+namespace {
+	namespace log      = boost::log;
+	namespace keywords = boost::log::keywords;
 
-template<typename CharT>
-struct AnsiColorFormatter {
-	log::formatter slave;
-	using return_type = void;
+	/// Formatter that invokes a slave formatter and adds ANSI color codes based on log severity.
+	template<typename Slave>
+	class AnsiColorFormatter {
+	protected:
+		Slave slave;
 
-	AnsiColorFormatter(log::formatter const & slave) : slave(slave) {}
-	AnsiColorFormatter(log::formatter && slave) : slave(std::move(slave)) {}
+	public:
+		/// Construct an AnsiColorFormatter with a slave formatter.
+		AnsiColorFormatter(Slave const & slave) : slave(slave) {}
 
-	void operator() (log::record_view const & record, log::basic_formatting_ostream<CharT> & stream) {
-		LogLevel severity = record["Severity"].extract_or_default<LogLevel, void, LogLevel>(LogLevel::info);
+		/// Construct an AnsiColorFormatter with a slave formatter.
+		AnsiColorFormatter(Slave && slave) : slave(std::move(slave)) {}
 
-		// Error and up is red.
-		if (severity >= LogLevel::error) {
-			stream << "\x1b[31m";
+		/// Format the record.
+		void operator() (log::record_view const & record, log::basic_formatting_ostream<char> & stream) {
+			LogLevel severity = record["Severity"].extract_or_default<LogLevel, void, LogLevel>(LogLevel::info);
 
-		// Warning is yellow.
-		} else if (severity >= LogLevel::warning) {
-			stream << "\x1b[33m";
+			// Add color code.
+			if (severity >= LogLevel::error) {
+				stream << "\x1b[31m";
+			} else if (severity >= LogLevel::warning) {
+				stream << "\x1b[33m";
+			} else if (severity == LogLevel::success) {
+				stream << "\x1b[32m";
+			} else {
+				stream << "\x1b[39m";
+			}
 
-		// Success is green.
-		} else if (severity == LogLevel::success) {
-			stream << "\x1b[32m";
+			// Invoke slave.
+			slave(record, stream);
 
-		// Rest is default color.
-		} else {
+			// Reset color.
 			stream << "\x1b[39m";
 		}
 
-		slave(record, stream);
+	};
 
-		// Reset color.
-		stream << "\x1b[39m";
+	/// Create an AnsiColorFormatter with a slave formatter.
+	template<typename Slave>
+	AnsiColorFormatter<typename std::decay<Slave>::type> makeAnsiColorFormatter(Slave && slave) {
+		return AnsiColorFormatter<typename std::decay<Slave>::type>(std::forward<Slave>(slave));
 	}
 
-};
+	/// Get a syslog severity mapper for dr::LogLevel.
+	log::sinks::syslog::custom_severity_mapping<LogLevel> severityMapping(std::string const & attribute_name = "Severity") {
+		log::sinks::syslog::custom_severity_mapping<LogLevel> mapping(attribute_name);
+		mapping[LogLevel::debug]    = log::sinks::syslog::debug;
+		mapping[LogLevel::info]     = log::sinks::syslog::info;
+		mapping[LogLevel::success]  = log::sinks::syslog::info;
+		mapping[LogLevel::warning]  = log::sinks::syslog::warning;
+		mapping[LogLevel::error]    = log::sinks::syslog::error;
+		mapping[LogLevel::critical] = log::sinks::syslog::critical;
+		return mapping;
+	};
+}
+
+std::ostream & operator<< (std::ostream & stream, LogLevel level) {
+	static char const * strings[] = {
+		"debug",
+		"info",
+		"success",
+		"warning",
+		"error",
+		"critical",
+	};
+
+	std::size_t numeric = int(level);
+	if (numeric < sizeof(strings) / sizeof(strings[0])) {
+		stream << strings[numeric];
+	} else {
+		stream << "unknown";
+	}
+
+	return stream;
+}
 
 
 void setupLogging(std::string const & base_dir, std::string const & name) {
 	log::core_ptr core = log::core::get();
 
+	// Get base and node directories.
 	boost::filesystem::path base = base_dir;
 	boost::filesystem::path node = base / name;
 
+	// Create directories if needed.
 	boost::system::error_code error;
 	boost::filesystem::create_directories(base, error);
 	boost::filesystem::create_directories(node, error);
 
+	// Add common attributes.
 	log::add_common_attributes();
+	core->add_global_attribute("Node", log::attributes::constant<std::string>(name));
 
-	auto text_format = log::expressions::format("[%1%] %2%")
-		% log::expressions::format_date_time<boost::posix_time::ptime>("TimeStamp", "%Y-%m-%d %H:%M:%S.%f")
-		% log::expressions::message;
+	// Text format for file and console log.
+	auto text_format = log::expressions::stream
+		<< "[" << log::expressions::format_date_time<boost::posix_time::ptime>("TimeStamp", "%Y-%m-%d %H:%M:%S.%f") << "] "
+		<< "[" << std::setw(8) << std::left << log::expressions::attr<LogLevel>("Severity") << "] "
+		<< "[" << log::expressions::attr<std::string>("Node") << "] "
+		<< log::expressions::message;
 
+	// Add file sink.
 	log::add_file_log(
 		keywords::file_name = (node / (name + ".log")).native(),
 		keywords::open_mode = std::ios_base::out | std::ios_base::app,
 		keywords::format    = text_format
 	);
 
-	log::add_console_log(std::clog)->set_formatter(AnsiColorFormatter<char>(text_format));
+	// Add console sink.
+	log::add_console_log(std::clog)->set_formatter(makeAnsiColorFormatter(text_format));
 
+	// Add syslog sink.
 	auto syslog = boost::make_shared<log::sinks::syslog_backend>(
 		keywords::facility = log::sinks::syslog::user,
 		keywords::use_impl = log::sinks::syslog::native
 	);
 
+	syslog->set_severity_mapper(severityMapping());
 	core->add_sink(boost::make_shared<log::sinks::synchronous_sink<log::sinks::syslog_backend>>(syslog));
 }
 
---
+++ src/test.cpp
@@ -0,0 +1,12 @@
+#include "dr_log.hpp"
+
+int main() {
+	dr::setupLogging("./", "test");
+	boost::log::sources::severity_logger<dr::LogLevel> logger;
+	BOOST_LOG_SEV(logger, dr::LogLevel::debug)    << "Debug";
+	BOOST_LOG_SEV(logger, dr::LogLevel::info)     << "Info";
+	BOOST_LOG_SEV(logger, dr::LogLevel::success)  << "Succes";
+	BOOST_LOG_SEV(logger, dr::LogLevel::warning)  << "Warning";
+	BOOST_LOG_SEV(logger, dr::LogLevel::error)    << "Error";
+	BOOST_LOG_SEV(logger, dr::LogLevel::critical) << "Critical";
+}
