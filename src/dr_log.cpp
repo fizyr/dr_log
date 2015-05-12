ec3630fa75e6a6176422a6979289b2759959f293
Maarten de Vries <maarten@de-vri.es>
2015-05-12 14:12:39 +0200

    Refactor sink creation.
    refs #871 @2h

--- src/dr_log.cpp
+++ src/dr_log.cpp
@@ -10,11 +10,11 @@
 #include <boost/log/utility/setup.hpp>
 #include <boost/log/sources/logger.hpp>
 #include <boost/log/support/date_time.hpp>
+#include <boost/log/utility/exception_handler.hpp>
 #include <boost/log/attributes/value_extraction.hpp>
 #include <boost/date_time/posix_time/posix_time_types.hpp>
 
 #include <boost/filesystem.hpp>
-
 
 namespace dr {
 
@@ -23,6 +23,15 @@ BOOST_LOG_GLOBAL_LOGGER_DEFAULT(dr_logger, Logger);
 namespace {
 	namespace log      = boost::log;
 	namespace keywords = boost::log::keywords;
+
+	boost::shared_ptr<log::sinks::sink> file_sink = nullptr;
+
+	void fileSinkExceptionHandler(std::exception const & e) {
+		log::core_ptr core = log::core::get();
+		core->remove_sink(file_sink);
+		file_sink = nullptr;
+		DR_ERROR("Error writing log file: " << e.what());
+	}
 
 	/// Formatter that invokes a slave formatter and adds ANSI color codes based on log severity.
 	template<typename Slave>
@@ -91,6 +100,61 @@ namespace {
 		mapping[LogLevel::critical] = log::sinks::syslog::critical;
 		return mapping;
 	};
+
+	// Text format for file and console log.
+	auto text_format = log::expressions::stream
+		<< "[" << log::expressions::format_date_time<boost::posix_time::ptime>("TimeStamp", "%Y-%m-%d %H:%M:%S.%f") << "] "
+		<< "[" << std::setw(8) << std::left << log::expressions::attr<LogLevel>("Severity") << "] "
+		<< "[" << log::expressions::attr<std::string>("Node") << "] "
+		<< log::expressions::message;
+
+	// Create a console sink.
+	boost::shared_ptr<log::sinks::synchronous_sink<log::sinks::basic_text_ostream_backend<char>>> createConsoleSink() {
+		auto backend = boost::make_shared<log::sinks::text_ostream_backend>();
+		backend->add_stream(boost::shared_ptr<std::ostream>(&std::clog, boost::null_deleter()));
+		auto frontend = boost::make_shared<log::sinks::synchronous_sink<log::sinks::text_ostream_backend>>(backend);
+		frontend->set_formatter(makeAnsiColorFormatter(text_format));
+		return frontend;
+	}
+
+	// Create a file sink that removes itself on exceptions and then logs the exception.
+	boost::shared_ptr<log::sinks::synchronous_sink<log::sinks::syslog_backend>> createSyslogSink() {
+		// Add syslog sink.
+		auto syslog = boost::make_shared<log::sinks::syslog_backend>(
+			keywords::facility = log::sinks::syslog::user,
+			keywords::use_impl = log::sinks::syslog::native
+		);
+
+		syslog->set_severity_mapper(severityMapping());
+		return boost::make_shared<log::sinks::synchronous_sink<log::sinks::syslog_backend>>(syslog);
+	}
+
+	// Create a syslog sink.
+
+	// Create a file sink that removes itself on exceptions and then logs the exception.
+	boost::shared_ptr<log::sinks::synchronous_sink<log::sinks::text_file_backend>> createFileSink(std::string const & filename) {
+		// Add file sink.
+		try {
+			auto file_frontend = boost::make_shared<log::sinks::synchronous_sink<log::sinks::text_file_backend>>(
+				keywords::file_name = filename,
+				keywords::open_mode = std::ios_base::out | std::ios_base::app,
+				keywords::format    = text_format
+			);
+
+			auto exception_handler = [filename, file_frontend] (std::exception const & e) {
+				log::core_ptr core = log::core::get();
+				log::core::get()->remove_sink(file_frontend);
+				DR_ERROR("Error writing log file: `" << filename << "' " << e.what());
+			};
+
+			file_frontend->set_exception_handler(log::make_exception_handler<std::exception>(exception_handler));
+			return file_frontend;
+
+		} catch (std::exception const & e) {
+			DR_ERROR("Failed to create log file `" << filename << "': " << e.what());
+			return nullptr;
+		}
+	}
 }
 
 std::ostream & operator<< (std::ostream & stream, LogLevel level) {
@@ -113,7 +177,6 @@ std::ostream & operator<< (std::ostream & stream, LogLevel level) {
 	return stream;
 }
 
-
 void setupLogging(std::string const & log_file, std::string const & name) {
 	log::core_ptr core = log::core::get();
 
@@ -121,37 +184,10 @@ void setupLogging(std::string const & log_file, std::string const & name) {
 	log::add_common_attributes();
 	core->add_global_attribute("Node", log::attributes::constant<std::string>(name));
 
-	// Text format for file and console log.
-	auto text_format = log::expressions::stream
-		<< "[" << log::expressions::format_date_time<boost::posix_time::ptime>("TimeStamp", "%Y-%m-%d %H:%M:%S.%f") << "] "
-		<< "[" << std::setw(8) << std::left << log::expressions::attr<LogLevel>("Severity") << "] "
-		<< "[" << log::expressions::attr<std::string>("Node") << "] "
-		<< log::expressions::message;
-
-	// Add console sink.
-	log::add_console_log(std::clog)->set_formatter(makeAnsiColorFormatter(text_format));
-
-	// Add syslog sink.
-	auto syslog = boost::make_shared<log::sinks::syslog_backend>(
-		keywords::facility = log::sinks::syslog::user,
-		keywords::use_impl = log::sinks::syslog::native
-	);
-
-	syslog->set_severity_mapper(severityMapping());
-	core->add_sink(boost::make_shared<log::sinks::synchronous_sink<log::sinks::syslog_backend>>(syslog));
-
-	Logger logger;
-
-	// Add file sink.
-	try {
-		log::add_file_log(
-			keywords::file_name = log_file,
-			keywords::open_mode = std::ios_base::out | std::ios_base::app,
-			keywords::format    = text_format
-		);
-	} catch (boost::filesystem::filesystem_error const & e) {
-		BOOST_LOG_SEV(logger, LogLevel::error) << "Failed to create log file `" << log_file << "': " << e.what();
-	}
+	// Add  sinks.
+	core->add_sink(createConsoleSink());
+	core->add_sink(createSyslogSink());
+	core->add_sink(createFileSink(log_file));
 }
 
 }
